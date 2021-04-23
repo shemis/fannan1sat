8221ac60991fb7cd189b1eda8fbbdb8067ffc0a5
unknown <ahmed.shemeas@gmail.com>
2021-04-23 23:50:04 +0200

    new 1

---
+++ new 1.js
@@ -0,0 +1,24 @@
+<script type='text/javascript'>
+ //<![CDATA[
+var allahref=document.getElementsByTagName("a");
+for(var c=0;c<allahref.length;c++)
+{
+if(allahref[c].hostname!="fannan1sat.blogspot.com" && allahref[c].hostname!="")
+if(allahref[c].hostname!="www.facebook.com" && allahref[c].hostname!="")
+if(allahref[c].hostname!="www.youtube.com" && allahref[c].hostname!="")
+if(allahref[c].hostname!="plus.google.com" && allahref[c].hostname!="")
+if(allahref[c].hostname!="www.instagram.com" && allahref[c].hostname!="")
+if(allahref[c].hostname!="twitter.com" && allahref[c].hostname!="")
+if(allahref[c].hostname!="1.bp.blogspot.com" && allahref[c].hostname!="")
+if(allahref[c].hostname!="2.bp.blogspot.com" && allahref[c].hostname!="")
+if(allahref[c].hostname!="3.bp.blogspot.com" && allahref[c].hostname!="")
+if(allahref[c].hostname!="4.bp.blogspot.com" && allahref[c].hostname!="")
+if(allahref[c].hostname!="www.blogger.com" && allahref[c].hostname!="")
+if(allahref[c].hostname!="www.linkedin.com" && allahref[c].hostname!="")
+if(allahref[c].hostname!="play.google.com" && allahref[c].hostname!="")
+{
+allahref[c].href="https://fannan1sat.blogspot.com/p/tahweel.html??&&url=_"+allahref[c].href;
+}
+ }
+ //]]>
+</script>
